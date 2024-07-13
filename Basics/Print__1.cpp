3f2b828b7e180e482fb1e317845bf5733b21365f
Tushar69k <tushlappy@gmail.com>
2024-07-13 21:49:16 +0530

    This is my CPP Progress

---
+++ Basics/Print__1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std ;
+int main(){
+cout << "Shree Ganeshayah Namah" << endl;
+return 0; }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+// cout << "Hello Mitr" << endl;
+//     return 0;
+// }
