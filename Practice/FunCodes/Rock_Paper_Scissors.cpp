519ba0ca31f9ad4978df1a5f5da5eba627aeed4d
Tushar69k <tushlappy@gmail.com>
2024-09-14 10:37:00 +0530

    14-09-2024

---
+++ Practice/FunCodes/Rock_Paper_Scissors.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    cout << "\n\t\t\t:: ROCK , PAPER , SCISSORS ::" << endl;
+    int UserInput;
+    cout << " 1. Select ROCK \n 2. Select PAPER \n 3. Select SCISSORS \n 4. Select Close Game\n";
+
+    do {
+        cout << "Your Choice : ";
+        cin >> UserInput;
+        if (UserInput == 1) {
+            cout << "( You Selects Rock )";
+        }
+
+        if (UserInput == 2) {
+            cout << "( You Selects Paper )";
+        }
+
+        if (UserInput == 3) {
+            cout << "( You Selects Scissors )";
+        }
+
+        if (UserInput == 4) {
+            cout << " GAME ENDED ";
+        }
+
+        Random random = new Random();
+        int ComInput = random.nextInt(1 , 4);
+
+        if (UserInput != 4) {
+            if (ComInput == 1) {
+                cout << "( Computer Selects Rock )";
+            }
+
+            if (ComInput == 2) {
+                cout << "( Computer Selects Paper )";
+            }
+
+            if (ComInput == 3) {
+                cout << "( Computer Selects Scissors )";
+            }
+        }
+
+        cout << endl;
+
+        if (UserInput == ComInput) {
+            cout << " Match Draw ! ";
+        }
+        else if (UserInput == 1 && ComInput == 3 || UserInput == 2 && ComInput == 1 || UserInput == 3 && ComInput == 2) {
+            cout << " Hurray ! You Win ";
+        }
+        else if (UserInput == 1 && ComInput == 2 || UserInput == 2 && ComInput == 3 || UserInput == 3 && ComInput == 1) {
+            cout << " Computer Wins ! ";
+        }
+        if (UserInput != 4) {
+            cout << "\n Wanna Try again ? If No Enter 4 ";
+        }
+    } while (UserInput != 4);
+
+    return 0;
+}
